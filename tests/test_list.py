c46c457cc9024e4b4a8ec23228fe6736c3f953ba
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2014-02-19 20:23:46 +0200

    added a couple of other edl tests for List.to_string() test cases.

--- tests/test_list.py
+++ tests/test_list.py
@@ -19,7 +19,7 @@ class ListTestCase(unittest.TestCase):
         """
         self.fail('test is not implemented yet')
 
-    def testing_to_edl_method_will_output_the_standard_edl(self):
+    def testing_to_edl_method_will_output_the_standard_edl_case1(self):
         """testing if to_string will output the EDL as string
         """
         p = edl.Parser('24')
@@ -27,6 +27,38 @@ class ListTestCase(unittest.TestCase):
             s = p.parse(f)
 
         with open('../tests/test_data/test_24.edl') as f:
+            expected_edl = f.readlines()
+
+        self.assertEqual(
+            ''.join(expected_edl),
+            s.to_string()
+        )
+
+    def testing_to_edl_method_will_output_the_standard_edl_case2(self):
+        """testing if to_string will output the EDL as string
+        """
+        p = edl.Parser('24')
+        with open('../tests/test_data/test.edl') as f:
+            s = p.parse(f)
+
+        with open('../tests/test_data/test.edl') as f:
+            expected_edl = f.readlines()
+
+        print s.to_string()
+
+        self.assertEqual(
+            ''.join(expected_edl),
+            s.to_string()
+        )
+
+    def testing_to_edl_method_will_output_the_standard_edl_case3(self):
+        """testing if to_string will output the EDL as string
+        """
+        p = edl.Parser('24')
+        with open('../tests/test_data/test_50.edl') as f:
+            s = p.parse(f)
+
+        with open('../tests/test_data/test_50.edl') as f:
             expected_edl = f.readlines()
 
         print s.to_string()
